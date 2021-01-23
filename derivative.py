2a5b7c7ac64db99f72c71e80bb961762a3a8c39a
Gaku Tokusumi <gakutokusumi@mac.com>
2021-01-23 12:05:00 -0500

    derivative calculator using sympy

--- derivative.py
+++ derivative.py
@@ -1,6 +1,19 @@
-from scipy.misc import derivative
+from sympy import *
 
-def f(x):
-    return x**2
+x = Symbol('x')
+y = Symbol('y')
+z = Symbol('z')
 
-print(derivative(f,1))
+
+def singDeriv(fx):
+    # x = Symbol('x')
+    return fx.diff(x)
+
+
+def multiDeriv(fx, wrF, wrS):
+    fxPrime = fx.diff(wrF)
+    fxPrime = fxPrime.diff(wrS)
+    return fxPrime
+
+print(multiDeriv(x**4+x*y**4,x,y))
+
