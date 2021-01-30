9ca1b1c9dd1e08f900e12570f4a66ce93fe8a7f6
Gaku Tokusumi <gakutokusumi@mac.com>
2021-01-29 19:15:49 -0500

    added sub, add, mul, neg function

--- linal.py
+++ linal.py
@@ -1,3 +1,5 @@
+import math
+
 class Vector3D:
     
     def __init__(self,x,y,z):
@@ -14,5 +16,20 @@ class Vector3D:
         second = self.x*other.z - self.z*other.z
         third = self.x*other.y - self.y*other.x
         return Vector3D(first - second + third)
+
+    def distance(self,other):
+        return math.sqrt((other.x-self.x)**2 + (other.y-self.y)**2 + (other.z-self.z)**2)
+
+    def __sub__(self,other):
+        return Vector3D(self.x-other.x,self.y-other.y,self.z-other.z)
+
+    def __add__(self,other):
+        return Vector3D(self.x+other.x,self.y+other.y,self.z+other.z)
+
+    def __mul__(self,scalar):
+        return Vector3D(self.x*scalar,self.y*scalar,self.z*scalar)
+
+    def __neg__(self):
+        return Vector3D(-self.x,-self.y,-self.z)
 
     
