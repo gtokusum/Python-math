4079c3fe5c8c4f42e643e570d7d251ddfdfab195
Gaku Tokusumi <gtokusum@iu.edu>
2021-09-08 16:59:08 -0400

    bisection method

---
+++ BisectionMethod.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+
+def bisection(f,a,b,tau,n):
+    if np.sign(f(a)) == np.sign(f(b)):
+        raise Exception("Root not in interval")
+    m = (a+b)/2
+    if np.abs(f(m)) < tau:
+        return m,n
+    elif np.sign(f(a)) == np.sign(f(m)):
+        return bisection(f, m, b, tau,n+1)
+    else:
+        return bisection(f,a,m,tau,n+1)
+
+if __name__ == "__main__":
+    fx = lambda x:x - 2*math.sin(x)
+    ans = bisection(fx,1.5,2.5,10**(-5),0)
+    print(ans)
+    # print(math.sin(1.5))
