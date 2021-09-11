b14bf73ec69e77b2263d783af32fd2ba4fea5393
Gaku Tokusumi <gtokusum@iu.edu>
2021-09-10 22:52:10 -0400

    bisection method

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -12,7 +12,31 @@ def bisection(f,a,b,tau,n):
     else:
         return bisection(f,a,m,tau,n+1)
 
+def fixedPoint(p0,f,n,stop):
+    newP = p0
+    while n != stop:
+        newP = f(newP)
+        print(newP, n)
+        n += 1
+    return
+
+def fixedIteration(p0,f,n,tau):
+    newP = f(p0)
+    print(newP,n)
+    while abs(newP - p0) > tau:
+        n += 1
+        p0 = newP
+        newP = f(newP)
+        print(newP,n)
+    return
+        
+
+
 if __name__ == "__main__":
-    fx = lambda x:x - 2*math.sin(x)
-    ans = bisection(fx,1.5,2.5,10**(-5),0)
-    print(ans)
+    # fx = lambda x:math.sqrt((2-x**4)/3)
+    # ans = bisection(fx,1.5,2.5,10**(-5),0)
+    # print(ans)
+    
+    fx = lambda x: 2+math.sin(x)
+    fixedIteration(2.5,fx,1,10**-5)
+    print(abs(2.5541921027478667-2.5542005781937362))
