e48f3e1878e119577f18f50437aa7c3e22207f52
Gaku Tokusumi <gtokusum@iu.edu>
2021-09-15 20:36:56 -0400

    Newton

--- NewtonMethod.py
+++ NewtonMethod.py
@@ -3,19 +3,23 @@ from scipy.misc import derivative
 
 
 
-def Newton(f,fprime,a,b,tau=10**-5):
+def Newton(f,fprime,a,tau=10**-5):
     n = 1
     p = 0
     p1 = a
     while abs(p-p1) > tau:
         p = p1
         p1 = p - f(p)/fprime(p)
-        print(n,p1)
+        # print(n,p1)
         n+=1
     return n,p1
 
-f = lambda x: math.e**x + 2**(-x) + 2*math.cos(x)-6
-fprime = lambda x: math.e**x - math.log(x)*2**(-2) - 2*math.sin(x)
-print(Newton(f,fprime,1,2)) 
+f = lambda x: 4*(x**2) - math.e**x - math.e**(-x)
+fprime = lambda x: 8*x -math.e**x + math.e**(-x)
+# print(Newton(f,fprime,-3)) 
+po = [-1,0,1,3,5,10]
 
-    
+for i in po:
+    print(i)
+    print(Newton(f,fprime,i))
+    print("\n\n")
