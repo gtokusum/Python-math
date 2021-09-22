6e75773347a7adbf4e13077d8cffd05b7c3f80a3
Gaku Tokusumi <gtokusum@iu.edu>
2021-09-22 13:55:33 -0400

    more methods to connect newton and aitken

--- AitkenDelta2.py
+++ AitkenDelta2.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import NewtonMethod
 
 def getPs(p0,pn):
     p1 = pn(p0)
@@ -11,14 +13,32 @@ def delta(p0,p1,p2,pn,n):
         return
     else:
         p1,p2=getPs(p0,pn)
+        # print(p1,p2,p0)
         q = p0 - (p1-p0)**2 /(p2 -2*p1 +p0)
         print(q)
         delta(p1,0,0,pn,n-1)
     return 
+e = math.e
+ln = math.log
 
 
-f = lambda x: (2-math.e**(x) +x**2)/3
-f2 = lambda x: (math.e**x/3)**(1/2)
+def deltaNewton(p0,q,f,fprime,tau,n=0):
+    p1 = NewtonMethod.getPs(f,fprime,p0)
+    p2 = NewtonMethod.getPs(f,fprime,p1)
+    q0 = p0 - (p1-p0)**2/(p2-2*p1+p0)
+    print(n,q0)
+    if abs(q-q0) > tau:
+        n += 1
+        deltaNewton(p1,q0,f,fprime,tau,n)
+    
+    return
 
+
+
+# f = lambda x: (2-math.e**(x) +x**2)/3
+# f2 = lambda x: (math.e**x/3)**(1/2)
+f = lambda x: e**(6*x) + 3*(ln(2))**2 * e**(2*x) - ln(8)*e**(4*x) - (ln(2))**3
+fprime = lambda x: 6*e**(6*x) + 6*(ln(2))**2*e**(2*x) - 12*(ln(2))*e**(4*x)
 # print(delta(0.5,0,0,f,5))
-print(delta(0.75,0,0,f2,5))
+
+deltaNewton(0,0,f,fprime,0.0002)
--- NewtonMethod.py
+++ NewtonMethod.py
@@ -2,24 +2,35 @@ import math
 from scipy.misc import derivative
 
 
+def getPs(f,fprime,a):
+    p = a
+    p1 = p - (f(p) /fprime(p))
+    return p1
+
 
 def Newton(f,fprime,a,tau=10**-5):
     n = 1
     p = 0
     p1 = a
     while abs(p-p1) > tau:
+        # print(abs(p-p1))
         p = p1
         p1 = p - f(p)/fprime(p)
         # print(n,p1)
         n+=1
     return n,p1
 
-f = lambda x: 4*(x**2) - math.e**x - math.e**(-x)
-fprime = lambda x: 8*x -math.e**x + math.e**(-x)
-# print(Newton(f,fprime,-3)) 
-po = [-1,0,1,3,5,10]
+e = math.e
+ln = math.log
 
-for i in po:
-    print(i)
-    print(Newton(f,fprime,i))
-    print("\n\n")
+
+
+f = lambda x: e**(6*x) + 3*(ln(2))**2 * e**(2*x) - ln(8)*e**(4*x) - (ln(2))**3
+fprime = lambda x: 6*e**(6*x) + 6*(ln(2))**2*e**(2*x) - 12*(ln(2))*e**(4*x)
+# print(Newton(f,fprime,-0.05113676593,0.0002)) 
+# po = [-1,0,1,3,5,10]
+# print(abs(-0.18288825721176558+0.18288825721176558))
+# for i in po:
+#     print(i)
+#     print(Newton(f,fprime,i))
+#     print("\n\n")
